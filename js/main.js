823e70d159a9078d98d669baddb02cca8e504cf0
unknown <dspehar@gmail.com>
2022-12-04 17:47:25 +0100

    Modification couture carroussel...

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+// Presse un bouton une photo apparaît:
+let photo = document.querySelector("#photo");
+
+function showPhoto() {
+    photo.innerHTML = `<img src="image/ventedeNoël.jpg" alt="couture" width="300" height="auto">`
+}
+
+
+// CARROUSEL:
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
